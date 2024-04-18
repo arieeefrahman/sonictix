164b9a53e4bf1b982cec76f90b73e5de29f6ad75
arieeefrahman <ariefrahman01@mail.ugm.ac.id>
2024-04-19 05:10:56 +0700

    feat(seeder): added new factory seeder for talent

---
+++ database/factories/TalentFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Talent;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Talent>
+ */
+class TalentFactory extends Factory
+{
+    protected $model = Talent::class;
+
+    public function definition(): array
+    {
+        return [
+            'stage_name' => fake()->name(),
+            'real_name' => fake()->name(),
+            'image_url' => fake()->url()
+        ];
+    }
+}
