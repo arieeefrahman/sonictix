cf489b83c244b2811b9c26782995bd2da4b7a456
arieeefrahman <ariefrahman01@mail.ugm.ac.id>
2024-05-03 18:15:03 +0700

    fix(test): fixed error when delete event talent not having timestamps

--- database/migrations/2024_04_16_151636_create_event_talents_table.php
+++ database/migrations/2024_04_16_151636_create_event_talents_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('event_talents', function (Blueprint $table) {
             $table->id();
+            $table->timestamps();
             $table->foreignId('event_id')->constrained(
                 table: 'events', indexName: 'event_talents_event_id_foreign'
             )->onDelete('cascade');
