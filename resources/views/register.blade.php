691120ef2b45b4c307955eb69c6f87e5995aaa11
arieeefrahman <ariefrahman01@mail.ugm.ac.id>
2024-05-31 04:14:23 +0700

    feat: added toggle password for more security

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -6,29 +6,34 @@
     <title>SonicTIX - Register</title>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 </head>
 <body>
+    @include('navbar')
     <div class="container">
         <div class="box form-box">
             <header>Register</header>
             <form id="register-form">
                 <div class="field input">
-                    <label for="username">Username</label>
+                    <label for="username" class="input-label">Username</label>
                     <input type="text" name="username" id="username" required>
                 </div>
 
                 <div class="field input">
-                    <label for="password">Password</label>
-                    <input type="password" name="password" id="password" required>
+                    <label for="password" class="input-label">Password</label>
+                    <div class="password-container">
+                        <input type="password" name="password" id="password" required>
+                        <i class="fas fa-eye" id="toggle-password"></i>
+                    </div>
                 </div>
 
                 <div class="field input">
-                    <label for="email">Email</label>
+                    <label for="email" class="input-label">Email</label>
                     <input type="email" name="email" id="email" required>
                 </div>
 
                 <div class="field input">
-                    <label for="full-name">Full Name</label>
+                    <label for="full-name" class="input-label">Full Name</label>
                     <input type="text" name="full-name" id="full-name" required>
                 </div>
 
