c0177918a17f9c41caf7647e811ab177af078425
arieeefrahman <ariefrahman01@mail.ugm.ac.id>
2024-05-31 04:15:48 +0700

    feat: included the navbar in the login page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,24 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SonicTIX - Login</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 <body>
+    @include('navbar')
     <div class="container">
         <div class="box form-box">
             <header>Login</header>
             <form id="login-form">
                 <div class="field input">
-                    <label for="username">Username</label>
-                    <input type="text" name="username" id="username" required>
+                    <input type="text" name="username" id="username" placeholder="Enter your username" required>
                 </div>
 
                 <div class="field input">
-                    <label for="password">Password</label>
-                    <!-- <input type="password" name="password" id="password" required> -->
                     <div class="password-container">
-                        <input type="password" name="password" id="password" required>
+                        <input type="password" name="password" id="password" placeholder="Enter your password" required>
                         <i class="fas fa-eye" id="toggle-password"></i>
                     </div>
                 </div>
