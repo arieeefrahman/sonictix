f5193061b881eca00e0b414f26878c63efea6e50
arieeefrahman <ariefrahman01@mail.ugm.ac.id>
2024-06-01 21:04:26 +0700

    feat: seeded event with ticket categories

--- database/seeders/EventSeeder.php
+++ database/seeders/EventSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Event;
+use App\Models\EventTicketCategory;
 use Illuminate\Database\Seeder;
 
 class EventSeeder extends Seeder
@@ -12,6 +13,13 @@ class EventSeeder extends Seeder
      */
     public function run(): void
     {
-        Event::factory()->count(10)->create();
+        Event::factory()
+            ->count(20)
+            ->create()
+            ->each(function ($event) {
+                EventTicketCategory::factory()
+                    ->count(3)
+                    ->create(['event_id' => $event->id]);
+        });
     }
 }
