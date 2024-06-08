bf46244d665603771ac72177e60f313c727361b0
arieeefrahman <ariefrahman01@mail.ugm.ac.id>
2024-06-08 17:03:09 +0700

    refactor: deleted double initiate called for sweetalert package

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -33,7 +33,6 @@
             </form>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="{{ asset('js/login.js') }}"></script>
 </body>
 </html>
