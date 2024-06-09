18a78dd0c1f6d8cba80c169db838545a9f3d357f
arieeefrahman <ariefrahman01@mail.ugm.ac.id>
2024-06-09 23:00:51 +0700

    fix: fixed bug the sweetalert did not show up because refactor

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SonicTIX - Login</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
@@ -33,6 +32,7 @@
             </form>
         </div>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="{{ asset('js/login.js') }}"></script>
 </body>
 </html>
