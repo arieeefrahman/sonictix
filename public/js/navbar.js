bd888019c5b8e71fd796deefe441df091813b3e1
arieeefrahman <ariefrahman01@mail.ugm.ac.id>
2024-06-10 13:53:46 +0700

    feat: added hamburger menu in navbar

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,57 +1,77 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var authToken = sessionStorage.getItem("authToken");
-    var loginLink = document.getElementById("login-link");
-    var logoutLink = document.getElementById("logout-link");
-    var registerLink = document.getElementById("register-link");
+            var authToken = sessionStorage.getItem("authToken");
+            var loginLink = document.getElementById("login-link");
+            var logoutLink = document.getElementById("logout-link");
+            var registerLink = document.getElementById("register-link");
+            var mobileLoginLink = document.getElementById("mobile-login-link");
+            var mobileLogoutLink = document.getElementById("mobile-logout-link");
+            var mobileRegisterLink = document.getElementById("mobile-register-link");
 
-    if (!authToken) {
-        // authToken is null, undefined, or an empty string
-        loginLink.classList.remove("hidden");
-        registerLink.classList.remove("hidden");
-        registerLink.classList.add("md:block");
-        logoutLink.classList.add("hidden");
-    } else {
-        // authToken is valid
-        loginLink.classList.add("hidden");
-        registerLink.classList.add("hidden");
-        registerLink.classList.remove("md:block");
-        logoutLink.classList.remove("hidden");
-    }
-});
+            if (!authToken) {
+                // authToken is null, undefined, or an empty string
+                loginLink.classList.remove("hidden");
+                registerLink.classList.remove("hidden");
+                registerLink.classList.add("md:block");
+                logoutLink.classList.add("hidden");
 
-function confirmLogout(event) {
-    event.preventDefault();
-    Swal.fire({
-        title: 'Are you sure to logout?',
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#0365b5',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes, log out!'
-    }).then((result) => {
-        if (result.isConfirmed) {
-            logout();
-        }
-    })
-}
+                mobileLoginLink.classList.remove("hidden");
+                mobileRegisterLink.classList.remove("hidden");
+                mobileLogoutLink.classList.add("hidden");
+            } else {
+                // authToken is valid
+                loginLink.classList.add("hidden");
+                registerLink.classList.add("hidden");
+                registerLink.classList.remove("md:block");
+                logoutLink.classList.remove("hidden");
 
-async function logout() {
-    try {
-        const response = await fetch('/api/logout', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${sessionStorage.getItem('authToken')}`
+                mobileLoginLink.classList.add("hidden");
+                mobileRegisterLink.classList.add("hidden");
+                mobileLogoutLink.classList.remove("hidden");
             }
         });
-        
-        if (response.ok) {
-            sessionStorage.removeItem('authToken');
-            window.location.href = "/home";
-        } else {
-            console.error('Logout failed');
+
+        const btn = document.getElementById('menu-btn');
+        const nav = document.getElementById('menu');
+
+        btn.addEventListener('click', () => {
+            btn.classList.toggle('open');
+            nav.classList.toggle('flex');
+            nav.classList.toggle('hidden');
+        });
+
+        function confirmLogout(event) {
+            event.preventDefault();
+            Swal.fire({
+                title: 'Are you sure to logout?',
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#0365b5',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Yes, log out!'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    logout();
+                }
+            });
         }
-    } catch (error) {
-        console.error('Error:', error);
-    }
-}
+
+        async function logout() {
+            try {
+                const response = await fetch('/api/logout', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${sessionStorage.getItem('authToken')}`
+                    }
+                });
+
+                if (response.ok) {
+                    sessionStorage.removeItem('authToken');
+                    window.location.href = "/home";
+                } else {
+                    console.error('Logout failed');
+                }
+            } catch (error) {
+                console.error('Error:', error);
+            }
+        }
--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -4,14 +4,14 @@
     <link rel="stylesheet" href="{{ asset('css/tailwind.css') }}">
 </head>
 <body>
-    
-    <nav class="mborder-solid border-2 border-black relative mx-auto px-8 py-4 bg-white">
+    <nav class="relative mx-auto px-6 py-4 bg-white">
         <!-- flex container -->
-        <div class="border-solid border-2 border-black flex items-center justify-between">
+        <div class="flex items-center justify-between">
             <!-- logo -->
-            <div class="border-solid border-2 border-black">
+            <div>
                 <img href="{{ url('/') }}" src="{{ asset('img/sonictix-logo.jpeg') }}" alt="SonicTIX Logo" class="h-10">
             </div>
+
             <!-- menu items -->
             <div class="hidden md:flex space-x-6 font-semibold">
                 <a href="{{ url('/') }}" class="p-3 px-6 pt-2">Home</a>
@@ -19,6 +19,23 @@
                 <a href="{{ url('/logout') }}" id="logout-link" class="p-3 px-6 pt-2 text-white bg-brightBlue rounded-full baseline" onclick="confirmLogout(event)">Logout</a>
                 <!-- register button -->
                 <a href="{{ url('/register') }}" id="register-link" class="block p-3 px-6 pt-2 text-white bg-brightBlue rounded-full baseline">Register</a>
+            </div>
+            
+            <!-- hamburger menu -->
+            <button id="menu-btn" class="block hamburger md:hidden ml-auto focus:outline-none">
+                <span class="hamburger-top"></span>
+                <span class="hamburger-mid"></span>
+                <span class="hamburger-bot"></span>
+            </button>
+
+            <!-- mobile menu -->
+            <div class="md:hidden">
+                <div id="menu" class="absolute flex-col items-center self-end hidden py-6 mt-10 space-y-6 font-bold bg-white sm:w-auto sm:self-center left-6 right-6 drop-shadow-md">
+                    <a href="#">Home</a>
+                    <a href="#" id="mobile-login-link">Login</a>
+                    <a href="#" id="mobile-logout-link">Logout</a>
+                    <a href="#" id="mobile-register-link">Register</a>
+                </div>
             </div>
         </div>
     </nav>
